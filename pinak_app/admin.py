3fa1d1b334a6f2f922aa8c6c72f46c78ccf3c10e
anilkathayat07 <kathayatanil3@gmail.com>
2024-11-25 19:22:39 +0530

    changes done

--- pinak_app/admin.py
+++ pinak_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from pinak_app.models import *
 
 # Register your models here.
+admin.site.register(Company_Details)
+admin.site.register(Bank_Details)
+admin.site.register(Machine_Types)
+admin.site.register(Company_Machines)
+admin.site.register(Working_Machines)
